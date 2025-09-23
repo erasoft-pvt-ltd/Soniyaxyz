644ac8953b1cf7fc25f41183bf393bc34cf499fa
atulpl1990 <atul.pal1990@gmail.com>
2025-09-23 13:11:09 -0700

    added about us page

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
-
+ servicesList = [
+    { title: 'Security Services', description: '...' },
+    { title: 'Detective & Private Investigation', description: '...' },
+    // etc.
+  ];
 }
