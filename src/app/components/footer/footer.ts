eed6e0b0af8aed37e27a56d42d69b0c86a800600
sunit_pal <sunit.pal@dellteam.com>
2025-09-26 03:34:59 +0530

    whatsapp link added

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
 
+  isVisible = false;
+
+  // Show button only after scrolling
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isVisible = (window.scrollY || document.documentElement.scrollTop || document.body.scrollTop) > 200;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
